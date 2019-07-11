3be29d08fd56e41b874e55790a2de509c5ece78f
hjdteny <hjd.teny@gmail.com>
2019-07-11 11:04:15 +1000

    change ver, updated GUI with 3 more file picker, renamed some constants

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ from esptool import NotImplementedInROMError
 from esptool import FatalError
 from argparse import Namespace
 
-__version__ = "4.0"
+__version__ = "1.0"
 __flash_help__ = '''
 <p>Modified to suit Speedshield VAM. Original help below: </p>
 <p>This setting is highly dependent on your device!<p>
@@ -66,15 +66,26 @@ class RedirectText:
 # ---------------------------------------------------------------------------
 
 
+class InvalidConfig(Exception):
+    pass
+
 # ---------------------------------------------------------------------------
 class FlashingThread(threading.Thread):
     def __init__(self, parent, config):
         threading.Thread.__init__(self)
         self.daemon = True
         self._parent = parent
+        if not FlashConfig.is_complete(config):
+            self._parent.report_error("config is incomplete \n%s" % config)
+            raise InvalidConfig("config is incomplete")
         self._config = config
+        
 
     def run(self):
+
+        def stringify(txt):
+            return "\"%s\"" % txt
+
         try:
             command = []
 
@@ -82,11 +93,28 @@ class FlashingThread(threading.Thread):
                 command.append("--port")
                 command.append(self._config.port)
 
-            command.extend(["--baud", str(self._config.baud),
-                            "--after", "no_reset",
+            command.extend([ "--chip", "esp32",
+                            "--baud", str(self._config.baud),
+                            "--before", "default_reset",
+                            "--after", "hard_reset",
                             "write_flash",
+                            "-z",
                             "--flash_mode", self._config.mode,
-                            "0x00000", self._config.firmware_path])
+                            "--flash_freq", "40m",
+                            "--flash_size", "detect"])
+
+            if (self._config.initialOTA_path is not None):
+                command.extend(["0xd000", self._config.initialOTA_path])
+
+            if (self._config.bootloader_path is not None):
+                command.extend(["0x1000", self._config.bootloader_path])
+            
+            if (self._config.firmware_path is not None):
+                command.extend(["0x100000", self._config.firmware_path])
+                
+            if (self._config.partition_path is not None):
+                command.extend(["0x8000", self._config.partition_path])
+                
 
             if self._config.erase_before_flash:
                 command.append("--erase-all")
@@ -99,8 +127,8 @@ class FlashingThread(threading.Thread):
             # done is needed
             print("\nFirmware successfully flashed. Unplug/replug or reset device \nto switch back to normal boot "
                   "mode.")
-        except SerialException as e:
-            self._parent.report_error(e.strerror)
+        except Exception as e:
+            self._parent.report_error(e)
             raise e
 
 
@@ -111,10 +139,13 @@ class FlashingThread(threading.Thread):
 # DTO between GUI and flashing thread
 class FlashConfig:
     def __init__(self):
-        self.baud = 115200
+        self.baud = 460800
         self.erase_before_flash = False
         self.mode = "dio"
         self.firmware_path = None
+        self.bootloader_path = None
+        self.initialOTA_path = None
+        self.partition_path = None
         self.port = None
 
     @classmethod
@@ -139,8 +170,14 @@ class FlashConfig:
         with open(file_path, 'w') as f:
             json.dump(data, f)
 
-    def is_complete(self):
-        return self.firmware_path is not None and self.port is not None
+    # def is_complete(self):
+    #     onePathIsDefined = (self.bootloader_path is not None) or (self.firmware_path is not None) or (self.initialOTA_path is not None) or (self.partition_path is not None)
+    #     return (onePathIsDefined) and self.port is not None
+    
+    @classmethod
+    def is_complete(self, config):
+        onePathIsDefined = (config.bootloader_path is not None) or (config.firmware_path is not None) or (config.initialOTA_path is not None) or (config.partition_path is not None)
+        return (onePathIsDefined) and config.port is not None
 
 # ---------------------------------------------------------------------------
 
@@ -197,14 +234,24 @@ class VamFlasher(wx.Frame):
             choice = event.GetEventObject()
             self._config.port = choice.GetString(choice.GetSelection())
 
-        def on_pick_file(event):
+        # file picker handler
+        def on_pick_bootloader_file(event):
+            self._config.bootloader_path = event.GetPath().replace("'", "")
+        
+        def on_pick_firmware_file(event):
             self._config.firmware_path = event.GetPath().replace("'", "")
+        
+        def on_pick_initialOTA_file(event):
+            self._config.initialOTA_path = event.GetPath().replace("'", "")
+        
+        def on_pick_partition_file(event):
+            self._config.partition_path = event.GetPath().replace("'", "")
 
         panel = wx.Panel(self)
 
         hbox = wx.BoxSizer(wx.HORIZONTAL)
 
-        fgs = wx.FlexGridSizer(7, 2, 10, 10)
+        fgs = wx.FlexGridSizer(10, 2, 10, 10)
 
         self.choice = wx.Choice(panel, choices=self._get_serial_ports())
         self.choice.Bind(wx.EVT_CHOICE, on_select_port)
@@ -215,8 +262,17 @@ class VamFlasher(wx.Frame):
         reload_button.Bind(wx.EVT_BUTTON, on_reload)
         reload_button.SetToolTip("Reload serial device list")
 
-        file_picker = wx.FilePickerCtrl(panel, style=wx.FLP_USE_TEXTCTRL)
-        file_picker.Bind(wx.EVT_FILEPICKER_CHANGED, on_pick_file)
+        file_bootloader_picker = wx.FilePickerCtrl(panel, style=wx.FLP_USE_TEXTCTRL)
+        file_bootloader_picker.Bind(wx.EVT_FILEPICKER_CHANGED, on_pick_bootloader_file)
+
+        file_firmware_picker = wx.FilePickerCtrl(panel, style=wx.FLP_USE_TEXTCTRL)
+        file_firmware_picker.Bind(wx.EVT_FILEPICKER_CHANGED, on_pick_firmware_file)
+
+        file_initialOTA_picker = wx.FilePickerCtrl(panel, style=wx.FLP_USE_TEXTCTRL)
+        file_initialOTA_picker.Bind(wx.EVT_FILEPICKER_CHANGED, on_pick_initialOTA_file)
+
+        file_partition_picker = wx.FilePickerCtrl(panel, style=wx.FLP_USE_TEXTCTRL)
+        file_partition_picker.Bind(wx.EVT_FILEPICKER_CHANGED, on_pick_partition_file)
 
         serial_boxsizer = wx.BoxSizer(wx.HORIZONTAL)
         serial_boxsizer.Add(self.choice, 1, wx.EXPAND)
@@ -268,7 +324,7 @@ class VamFlasher(wx.Frame):
         add_erase_radio_button(erase_boxsizer, 0, False, "no", erase is False)
         add_erase_radio_button(erase_boxsizer, 1, True, "yes, wipes all data", erase is True)
 
-        button = wx.Button(panel, -1, "Flash NodeMCU")
+        button = wx.Button(panel, -1, "Flash VAM")
         button.Bind(wx.EVT_BUTTON, on_clicked)
 
         self.console_ctrl = wx.TextCtrl(panel, style=wx.TE_MULTILINE | wx.TE_READONLY | wx.HSCROLL)
@@ -279,7 +335,12 @@ class VamFlasher(wx.Frame):
         self.console_ctrl.SetDefaultStyle(wx.TextAttr(wx.BLUE))
 
         port_label = wx.StaticText(panel, label="Serial port")
-        file_label = wx.StaticText(panel, label="NodeMCU firmware")
+
+        file_bootloader_label = wx.StaticText(panel, label="Bootloader")
+        file_firmware_label = wx.StaticText(panel, label="Firmware")
+        file_initialOTA_label = wx.StaticText(panel, label="Initial OTA")
+        file_partition_label = wx.StaticText(panel, label="Partition Table")
+
         baud_label = wx.StaticText(panel, label="Baud rate")
         flashmode_label = wx.StaticText(panel, label="Flash mode")
 
@@ -307,13 +368,16 @@ class VamFlasher(wx.Frame):
 
         fgs.AddMany([
                     port_label, (serial_boxsizer, 1, wx.EXPAND),
-                    file_label, (file_picker, 1, wx.EXPAND),
+                    file_bootloader_label, (file_bootloader_picker, 1, wx.EXPAND),
+                    file_firmware_label, (file_firmware_picker, 1, wx.EXPAND),
+                    file_initialOTA_label, (file_initialOTA_picker, 1, wx.EXPAND),
+                    file_partition_label, (file_partition_picker, 1, wx.EXPAND),
                     baud_label, baud_boxsizer,
                     flashmode_label_boxsizer, flashmode_boxsizer,
                     erase_label, erase_boxsizer,
                     (wx.StaticText(panel, label="")), (button, 1, wx.EXPAND),
                     (console_label, 1, wx.EXPAND), (self.console_ctrl, 1, wx.EXPAND)])
-        fgs.AddGrowableRow(6, 1)
+        fgs.AddGrowableRow(9, 1)
         fgs.AddGrowableCol(1, 1)
         hbox.Add(fgs, proportion=2, flag=wx.ALL | wx.EXPAND, border=15)
         panel.SetSizer(hbox)
@@ -339,7 +403,7 @@ class VamFlasher(wx.Frame):
     def _build_status_bar(self):
         self.statusBar = self.CreateStatusBar(2, wx.STB_SIZEGRIP)
         self.statusBar.SetStatusWidths([-2, -1])
-        status_text = "Welcome to NodeMCU PyFlasher %s" % __version__
+        status_text = "Welcome to %s %s" % (__title__,__version__)
         self.statusBar.SetStatusText(status_text, 0)
 
     def _build_menu_bar(self):
@@ -348,14 +412,14 @@ class VamFlasher(wx.Frame):
         # File menu
         file_menu = wx.Menu()
         wx.App.SetMacExitMenuItemId(wx.ID_EXIT)
-        exit_item = file_menu.Append(wx.ID_EXIT, "E&xit\tCtrl-Q", "Exit NodeMCU PyFlasher")
+        exit_item = file_menu.Append(wx.ID_EXIT, "E&xit\tCtrl-Q", "Exit Speedshield Vam Flash Tool")
         exit_item.SetBitmap(images.Exit.GetBitmap())
         self.Bind(wx.EVT_MENU, self._on_exit_app, exit_item)
         self.menuBar.Append(file_menu, "&File")
 
         # Help menu
         help_menu = wx.Menu()
-        help_item = help_menu.Append(wx.ID_ABOUT, '&About NodeMCU PyFlasher', 'About')
+        help_item = help_menu.Append(wx.ID_ABOUT, '&About Speedshield VAM Flash Tool', 'About')
         self.Bind(wx.EVT_MENU, self._on_help_about, help_item)
         self.menuBar.Append(help_menu, '&Help')
 
@@ -363,7 +427,7 @@ class VamFlasher(wx.Frame):
 
     @staticmethod
     def _get_config_file_path():
-        return wx.StandardPaths.Get().GetUserConfigDir() + "/nodemcu-pyflasher.json"
+        return wx.StandardPaths.Get().GetUserConfigDir() + "/vam-flasher.json"
 
     # Menu methods
     def _on_exit_app(self, event):
